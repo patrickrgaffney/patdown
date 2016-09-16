d920a1f8a732b3e0dbb9f632e0521ca6cf461d4e
Pat Gaffney <pat@hypepat.com>
2016-09-15 20:03:05 -0500

    UPDATE: documentation on error throwing functions

--- errors.c
+++ errors.c
@@ -1,20 +1,22 @@
-/* 
- * errors.c -- methods for throwing errors.
+/**
+ * errors.c -- methods for throwing errors
  * 
- * Created by PAT GAFFNEY on 06/15/2016.
- * Last modified on 06/15/2016.
+ * @author      Pat Gaffney <pat@hypepat.com>
+ * @created     2016-06-15
+ * @modified    2016-09-15
  * 
- *********ultrapatbeams*/
+ ************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "errors.h"
 
-/******************************************************************
+/**
  * throw_memory_error() -- memory could not be allocated
- *
+ **
  * @fatal -- exits program with error dialog
- ******************************************************************/
+ ************************************************************************/
 void throw_memory_error(void)
 {
     fprintf(stderr, "FATAL: memory could not be allocated.\n");
@@ -22,56 +24,53 @@ void throw_memory_error(void)
 }
 
 
-/******************************************************************
- * throw_file_opening_error() -- file could not be opened
- * 
- * const char *filename -- the name of the unopened file
- *
+/**
+ * throw_file_opening_error(file) -- file could not be opened
+ **
  * @fatal -- exits program with error dialog
- ******************************************************************/
-void throw_file_opening_error(const char *fileName)
+ ************************************************************************/
+void throw_file_opening_error(const char *file)
 {
-    fprintf(stderr, "FATAL: \'%s\' could not be opened.\n", fileName);
+    fprintf(stderr, "FATAL: \'%s\' could not be opened.\n", file);
     exit(EXIT_FAILURE);
 }
 
 
-/******************************************************************
- * throw_invalid_option_error() -- provided option flag is invalid
- *
- * const char *argument -- the invalid option flag
- *
+/**
+ * throw_multiple_input_files_error() -- multiple input files provided
+ **
  * @fatal -- exits program with error dialog
- ******************************************************************/
-void throw_invalid_option_error(const char *argument)
-{
-    fprintf(stderr, "ERROR: \'%s\' is an invalid option.\n", argument);
-    exit(EXIT_FAILURE);
-}
-
-
-/******************************************************************
- * throw_invalid_output_t_error() -- output type is invalid
- *
- * const char *argument -- the invalid output type
- *
- * @fatal -- exits program with error dialog
- ******************************************************************/
-void throw_invalid_output_t_error(const char *argument)
-{
-    fprintf(stderr, "ERROR: \'%s\' is an invalid output type.\n", argument);
-    exit(EXIT_FAILURE);
-}
-
-
-/******************************************************************
- * throw_multiple_input_files_error() -- multiple input files 
- *                                       provided
- *
- * @fatal -- exits program with error dialog
- ******************************************************************/
+ ************************************************************************/
 void throw_multiple_input_files_error(void)
 {
     fprintf(stderr, "FATAL: Multiple input files were provided.\n");
     exit(EXIT_FAILURE);
 }
+
+
+// /*
+//  * throw_invalid_option_error() -- provided option flag is invalid
+//  *
+//  * const char *argument -- the invalid option flag
+//  *
+//  * @fatal -- exits program with error dialog
+//  *****************************************************************/
+// void throw_invalid_option_error(const char *arg)
+// {
+//     fprintf(stderr, "ERROR: \'%s\' is an invalid option.\n", argument);
+//     exit(EXIT_FAILURE);
+// }
+//
+//
+// /******************************************************************
+//  * throw_invalid_output_t_error() -- output type is invalid
+//  *
+//  * const char *argument -- the invalid output type
+//  *
+//  * @fatal -- exits program with error dialog
+//  ******************************************************************/
+// void throw_invalid_output_t_error(const char *argument)
+// {
+//     fprintf(stderr, "ERROR: \'%s\' is an invalid output type.\n", argument);
+//     exit(EXIT_FAILURE);
+// }
--- errors.h
+++ errors.h
@@ -1,57 +1,22 @@
-/* 
- * errors.h -- methods for throwing errors.
+/***** 
+ * errors.h -- methods for throwing errors
  * 
- * Created by PAT GAFFNEY on 06/15/2016.
- * Last modified on 06/15/2016.
+ * @author      Pat Gaffney <pat@hypepat.com>
+ * @created     2016-06-15
+ * @modified    2016-09-15
  * 
- *********ultrapatbeams*/
+ ************************************************************************/
 
 #ifndef ERRORS_H
 #define ERRORS_H
 
-/******************************************************************
- * throw_memory_error() -- memory could not be allocated
- * 
- * @fatal -- exits program with error dialog
- ******************************************************************/
+/* Memory could not be allocated */
 void throw_memory_error(void);
 
-
-/******************************************************************
- * throw_file_opening_error() -- file could not be opened
- *
- * @fatal -- exits program with error dialog
- ******************************************************************/
+/* File could not be opened */
 void throw_file_opening_error(const char *fileName);
 
-
-/******************************************************************
- * throw_invalid_option_error() -- provided option flag is invalid
- *
- * const char *argument -- the invalid option flag
- *
- * @fatal -- exits program with error dialog
- ******************************************************************/
-void throw_invalid_option_error(const char *argument);
-
-
-/******************************************************************
- * throw_invalid_output_t_error() -- output type is invalid
- *
- * const char *argument -- the invalid output type
- *
- * @fatal -- exits program with error dialog
- ******************************************************************/
-void throw_invalid_output_t_error(const char *argument);
-
-
-/******************************************************************
- * throw_multiple_input_files_error() -- multiple input files 
- *                                       provided
- *
- * @fatal -- exits program with error dialog
- ******************************************************************/
+/* Multiple input files provided */
 void throw_multiple_input_files_error(void);
-
 
 #endif
