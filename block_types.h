5a3e033f6cdf67bdd2f4abe3ea736b9b239594e3
Pat Gaffney <pat@hypepat.com>
2016-09-15 19:27:07 -0500

    UPDATE: clean up block_types.h
    This file has essentially been given a death wish. The plan is to move md_code_block_t to markdown.h and move link_ref_t to a new file: links.(c|h).

--- block_types.h
+++ block_types.h
@@ -1,32 +1,42 @@
+/***** 
+ * block_types.h -- extensions to basic markdown_t node
+ * 
+ * @author      Pat Gaffney <pat@hypepat.com>
+ * @created     2016-07-28
+ * @modified    2016-09-15
+ * 
+ *	TODO: Move all link references and function definitions to their own
+ *		  file -- links.(c|h)
+ *
+ *	TODO: Move md_code_block_t to markdown.h file -- its functions are
+ *		  already there.
+ ************************************************************************/
+
 #ifndef BLOCK_TYPES_H
 #define BLOCK_TYPES_H
 
 #include <stddef.h>
 #include <stdbool.h>
-#include "markdown.h"
 
+/**
+ * md_code_block_t -- markdown_t extension for fenced code blocks
+ ************************************************************************/
 typedef struct md_code_block_t
 {
-    char lang[20]; // info string from opening fence
+    char lang[20];		/* User-provided language for the code block. 	*/
 } md_code_block_t;
 
-typedef struct md_blockquote_t
-{
-    size_t level; // level of nested blockquote
-} md_blockquote_t;
 
-typedef struct md_list_t
-{
-    bool ordered; // true for <ol>, false for <ul>
-    char marker; // marker character used
-    size_t indentation; // level of currently nested list
-} md_list_t;
-
+/**
+ * link_ref_t -- node to hold link data information
+ **
+ *	These nodes will be used to create a BST of link references.
+ ************************************************************************/
 typedef struct link_ref_t
 {
-    char link[1000];
-    char dest[1000];
-    char title[1000];
+    char link[1000];	/* Link label -- unique identifier for links. 	*/
+    char dest[1000];	/* Link destination -- the URL to link to.		*/
+    char title[1000];	/* Link title -- optional, <a title> attribute.	*/
 } link_ref_t;
 
 #endif
