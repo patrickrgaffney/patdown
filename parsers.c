patrickrgaffney/patdown

commit 913c28dc580c4865aff9ac9ea4b29e25f794e174

Fix fenced code blocks to remove leading WS
When the opening fence is parsed, the amount of leading WS on the
line with that fence is saved. That amount is then removed from all
subsequent lines of the block -- if they contain leading WS.

parsers.c
@@ -579,7 +579,7 @@ static size_t parse_fenced_code_block(uint8_t *data, CodeBlk *blk, size_t i)
         
         /* Advance past the WS on the opening code fence. */
         size_t linews = 0;
-        if (blk->ws > 0 && ws > 0 && linews < blk->ws) {
+        while (blk->ws > 0 && ws > 0 && linews < blk->ws) {
             data++, i++;
             linews++;
         }

commit 44f3a85f24e2fb119396ea7020f9ddff67cc6506

Fix leading WS on opening code fences
This fixes an error that was introduced in 913c28d.

parsers.c
@@ -564,14 +564,12 @@ static ssize_t is_closing_code_fence(uint8_t *data, CodeBlk *blk)
  */
 static size_t parse_fenced_code_block(uint8_t *data, CodeBlk *blk, size_t i)
 {
-    String *cb = init_string(BLK_BUF);
-    size_t ws  = 0;
+    String *cb  = init_string(BLK_BUF);
     ssize_t cfl = 0;    /* Closing-fence line length (in bytes). */
     
     /* Parse every line as part of this code block 
      * until we find the closing fence. */
     while (true) {
-        ws = count_indentation(data);
         
         /* Check this line for a code fence. */
         if ((cfl = is_closing_code_fence(data, blk)) > 0) break;
@@ -579,7 +577,7 @@ static size_t parse_fenced_code_block(uint8_t *data, CodeBlk *blk, size_t i)
         
         /* Advance past the WS on the opening code fence. */
         size_t linews = 0;
-        while (blk->ws > 0 && ws > 0 && linews < blk->ws) {
+        while (blk->ws > 0 && *data == 0x20 && linews < blk->ws) {
             data++, i++;
             linews++;
         }